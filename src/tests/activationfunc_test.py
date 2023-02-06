186889a594a069a5ea58082143f1f94909087794
saavajuu <juuso.saavalainen@helsinki.fi>
2023-02-06 03:25:07 +0200

    tests for the activation functions

---
+++ src/tests/activationfunc_test.py
@@ -0,0 +1,36 @@
+import unittest
+import numpy as np
+from model.utility import relU, drelU, sigmoid, dsigmoid, softmax
+
+
+class TestActivationFunctions(unittest.TestCase):
+    def setUp(self):
+        self.Z = np.array([[-1, 0], [1, -1], [0, 1]])
+
+    def test_relU(self):
+        expected = np.array([[0, 0], [1, 0], [0, 1]])
+        np.testing.assert_array_equal(relU(self.Z), expected)
+
+    def test_drelU(self):
+        expected = np.array([[0, 0], [1, 0], [0, 1]])
+        np.testing.assert_array_equal(drelU(self.Z), expected)
+
+    def test_sigmoid(self):
+        expected = np.array(
+            [[0.26894142, 0.5], [0.73105858, 0.26894142], [0.5, 0.73105858]])
+        np.testing.assert_allclose(sigmoid(self.Z), expected, rtol=1e-05)
+
+    def test_dsigmoid(self):
+        expected = np.array(
+            [[0.19661193, 0.25], [0.19661193, 0.19661193], [0.25, 0.19661193]])
+        np.testing.assert_allclose(dsigmoid(self.Z), expected, rtol=1e-05)
+
+    def test_softmax(self):
+        Z = np.array([1, 2, 3, 4, 1, 2, 3])
+        expected = np.array([0.02364054, 0.06426166, 0.1746813,
+                            0.474833, 0.02364054, 0.06426166, 0.1746813])
+        np.testing.assert_allclose(softmax(Z), expected, rtol=1e-6, atol=1e-6)
+
+
+if __name__ == '__main__':
+    unittest.main()
